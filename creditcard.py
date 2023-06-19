20053e20ea3ff54b5fa85aa5723f1b223aed70a5
Imaadu Deen Tawiah <106468069+Spartachus@users.noreply.github.com>
2023-06-19 14:14:05 +0200

    Update creditcard.py

--- creditcard.py
+++ creditcard.py
@@ -1,18 +1,17 @@
 def cardchecker(card_num):
-  if card_num[0] == 4 and len(card_num) == 16:
+  if card_num[0] == "4" and len(card_num) == 16 or 13:
     validity = "Visa Card"
-  elif card_num[0] == 5 and len(card_num) == 16:
+  elif card_num[0] == "5" and len(card_num) == 16:
     validity = "Master Card"
-  elif card_num[0] == 3:
-    if card_num[1] == 4 or card_num[2] == 7:
+  elif card_num[0] == "3":
+    if card_num[1] == "4" or card_num[2] == "7":
       if len(card_num) == 15:
         validity = "American Express"
-  elif card_num[0] == 6 and len(card_num) == 16:
+  elif card_num[0] == "6" and len(card_num) == 16:
     validity = "Discover Card"
   else:
     validity = "Invalid Card Number"
   return validity
 
 usr_num = input("Enter Your Card Number:\n")
-print(len(usr_num))
 print(cardchecker(usr_num))
